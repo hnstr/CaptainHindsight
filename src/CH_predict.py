be19135ba1b56f529759452156f02b592a34b069
Ward Heij <wardheij@gmail.com>
2016-12-09 13:25:07 +0100

    Updated prediction

--- src/CH_predict.py
+++ src/CH_predict.py
@@ -1,15 +1,36 @@
+import numpy as np
 
-def predict_caption(model, image_feature):
 
-    # Preallocate empty array
-    predictions = []
+def predict_caption(model, image_feature, word2int, int2word):
 
-    for i in range(len(image_features)):
-        feature = image_features[i, :]
-        # Predict new output based on single image feature vector
-        # TODO: update batch size
-        prediction = predict(model, feature, batch_size=32, verbose=0)
+    start_token = np.ones((np.size(image_feature), 1)) * word2int['<s>']
 
-        predictions.append(prediction)
+    previous_token = start_token
+    end_of_sentence = False
+    sentence_length = 0
 
-    return predictions
+    predicted_sentence = []
+
+
+    while not end_of_sentence:
+        prediction = model.predict([image_feature, previous_token], batch_size=32, verbose=1)
+
+        encoded_word = np.argmax(prediction)
+
+        # Naive sampling of sentence
+        if (encoded_word == word2int['</s>']):
+            end_of_sentence = True
+            print('End of sentence marker encountered')
+        elif (sentence_length > 60):
+            end_of_sentence = True
+            print('Max sentence length reached')
+        else:
+            sentence_length += 1
+            # print(encoded_word)
+            print(int2word[encoded_word])
+            previous_token = np.ones((np.size(image_feature), 1)) * encoded_word
+            predicted_sentence.append(encoded_word)
+
+    # predictions.append(prediction)
+
+    return predicted_sentence
